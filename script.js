c525db0db30883bd0a19520503fb1facfe4aca23
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 12:51:01 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// CODE ALONG
+// Console log script running
+console.log("Script running")
+
+// Console log the document
+console.log(document)
+
+// Console log the innerHTML of the the document's head.
+console.log(document["head"].innerHTML)
+
+// Select the img tag for the vanilla ice cream and save it in a variable
+let vanilla = document.getElementById("vanillaImage")
+let vanilla2 = document.querySelector("#vanillaImage")
+
+// Console log the variable to confirm you selected it correctly!
+console.log(vanilla2)
+
+// CODING EXERCISES
+// Task 1: 
+// Console log the title of the document.
+console.log(document["head"].title)
+
+// Task 2: 
+// Console log the className of the the document's body.
+console.log(document["body"].)
+
+
+// Task 3: 
+// Select the img tag for the chocolate ice cream and save it in a variable
+
+
+// Console log the variable to confirm you selected it correctly!
+
+
+// Console log the src of the variable to see the image image address
+
+
+// Task 4: 
+// Select the h1 tag and save it in a variable
+
+
+// Console log the variable to confirm you selected it correctly!
+
+
+
+// Task 5: Add an ID to the mint chocolate chip price in HTML
+// Select the price and save it in a vairable
+
+
+// Console log the variable to confirm you selected it correctly!
+
+
+// STRETCH: 
+// So far we have only selected one element at a time. Use Google and try to select all the images at once and save it in a variable.
+
+
+// Console log the output to see what is returned!
+
